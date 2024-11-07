b0894742c9f7b3480b4019659d5d3d7eccba90dd
Carlos Delgado <megaloto.2024@gmail.com>
2024-11-06 22:40:14 -0500

    update 2.0.8 inicio sorteo 7 pm 4

--- serverFull.js
+++ serverFull.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
 
   let count = 0;
 /sorteo express/////
-cron.schedule('37 22 * * *', () => {
+cron.schedule('45 22 * * *', () => {
   console.log('entro al reset')
   resetExpress()
 })
